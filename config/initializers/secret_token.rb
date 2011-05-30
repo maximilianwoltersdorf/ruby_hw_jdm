b22d6483746405e85b3e8c43ded2f8423da7a854
maximilianwoltersdorf <m.woltersdorf@hotmail.de>
2011-05-30 16:35:02 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyHwJdm::Application.config.secret_token = '62caa1584df3ae5d87c5f296b455496c333c4e9744fce03ceadfcf52d6a93437ac5ba573a2de5658196c919613c93c521c4615b20d4c1719c964804d4469f971'
