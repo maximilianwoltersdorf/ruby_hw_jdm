01d7aa0a48744e312638120a676cd1b60cad4592
Julian Joswig <julian.joswig@joswig-it.de>
2011-06-06 15:05:59 +0200

    finished modelling users

---
+++ db/migrate/20110606130442_add_uniqueness_to_email.rb
@@ -0,0 +1,9 @@
+class AddUniquenessToEmail < ActiveRecord::Migration
+  def self.up
+    add_index :users, :email, :uniqueness => true
+  end
+
+  def self.down
+    remove_index :users, :email
+  end
+end
