7a52b4476f3f4c0c9ee367bb5fa6841046a3b6e9
maximilianwoltersdorf <m.woltersdorf@hotmail.de>
2011-06-08 16:05:04 +0200

    signup -db wirft fehler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   def login
     @title = "Login"
   end
@@ -8,9 +9,24 @@ class UsersController < ApplicationController
   end
   
   def show
-    
     @user = User.find(params[:id])
     @title = @user.name
+  end
+  
+  def signup
+    @user = User.new
+    @title = "Sign up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Wilkommen bei Portalspass!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'signup'
+    end
   end
 
 end
