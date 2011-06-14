86803a6b702a77e3b4934032fbdb3981a22c32d2
Julian Joswig <julian.joswig@joswig-it.de>
2011-06-14 15:32:37 +0200

    jQTouch integriert, Vorbereitungen für Mobilversion getroffen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper :all
+  
+  before_filter :prepare_for_mobile
   
   def after_sign_in_path_for(resource)
    "/show"
    if @user.save
      UserMailer.deliver_registration_confirmation(@user)
+   end
   end
-end  
+  
+  private
+  
+    def mobile_device?
+      if session[:mobile_param]
+        session[:mobile_param] == "1"
+      else
+        request.user_agent =~ /Mobile|webOS/
+      end
+    end
+    
+    def prepare_for_mobile
+      session[:mobile_param] = params[:mobile] if params[:mobile]  
+      request.format = :mobile if mobile_device?
+    end
+    
+    helper_method :mobile_device?  
 end
