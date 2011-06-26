f183ade4714f2b517ce31437a03c643d5961d82c
Julian Joswig <julian.joswig@joswig-it.de>
2011-06-26 16:40:43 +0200

    fertig

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -8,11 +8,13 @@ module DashboardsHelper
           rss = SimpleRSS.parse open(widget.sourceurl)
         
           if rss.items        
-            output = "<h2>" + rss.channel.title + "</h2>"
-                    
+            output = '<h2>' + image_tag('bullet_toggle_plus.png', { :onclick => "new Effect.toggle('" + widget.id.to_s + "');"}) + rss.channel.title + '</h2>'
+            
+            output += "<div id='" + widget.id.to_s + "'>"        
             rss.items[0..4].each do |i|
               output += "<h3>" + link_to(i.title, i.link, { :target => '_blank' }) + "</h3>"
             end
+            output += '</div>'
             
             # Quelle: http://blog.emeidi.com/2011/02/25/ruby-on-rails-3-kleinigkeiten/
             output.html_safe
